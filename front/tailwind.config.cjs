5788a4c27f73bd5306b2beebf2bc207d6d13d573
filipesaretta <filipessm124@gmail.com>
2023-01-27 10:32:27 -0300

    Extend colors

--- front/tailwind.config.cjs
+++ front/tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['../index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-coral': '#EE7674',
+        'maximum-blue-green': '#75B9BE',
+        'rich-black': '#011627',
+        'yellow-orange': '#FBB13C',
+        'lavender-blue-400': '#D1C9FF',
+        'lavender-blue-800': '#A899FF',
+      },
+    },
   },
   plugins: [],
 }
